80cc16e5ed74fa8f10486834f6f46068193ba32a
jeehoonK-coder <64194416+jeehoonK-coder@users.noreply.github.com>
2020-06-11 19:29:20 +0900

    back

---
+++ app/src/main/java/com/team_comfortable/forhealthylife/ui/exercise/BackFragment.java
@@ -0,0 +1,56 @@
+package com.team_comfortable.forhealthylife.ui.exercise;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.team_comfortable.forhealthylife.R;
+import com.team_comfortable.forhealthylife.ui.exercise.ExerciseViewModel;
+
+public class BackFragment extends Fragment {
+
+    private ExerciseViewModel exerciseViewModel;
+
+    public static BackFragment newInstance() {
+        return new BackFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_back, container, false);
+        ListView listview = root.findViewById(R.id.backName);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, DataExercise.backName);
+        listview.setAdapter(adapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                exerciseViewModel.setInteger(position+6);
+            }
+        });
+
+        return root;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        exerciseViewModel = ViewModelProviders.of(getActivity()).get(ExerciseViewModel.class);
+
+
+
+    }
+
+}
