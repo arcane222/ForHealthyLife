8350d61e89934d9097d77f0b960d4b3633a8d0cb
jeehoonK-coder <64194416+jeehoonK-coder@users.noreply.github.com>
2020-06-14 04:09:12 +0900

    add enterbtn and cancelbtn

--- app/src/main/java/com/team_comfortable/forhealthylife/ui/calendar/fragment/ScheduleFragment.java
+++ app/src/main/java/com/team_comfortable/forhealthylife/ui/calendar/fragment/ScheduleFragment.java
@@ -1,65 +1,160 @@
 package com.team_comfortable.forhealthylife.ui.calendar.fragment;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.team_comfortable.forhealthylife.R;
+import com.team_comfortable.forhealthylife.ui.eating.RiceFragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ScheduleFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ScheduleFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.util.HashMap;
+import java.util.Map;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public ScheduleFragment() {
-        // Required empty public constructor
+public class ScheduleFragment extends Fragment{
+
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mReference;
+    private FirebaseUser mUser;
+    private FirebaseAuth mAuth;
+    private String schedule;
+    private String[] scheduleList = {"1", "2","12"};
+
+    public void initFirebase()
+    {
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance();
+        mReference = mDatabase.getReference();
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ScheduleFragment.
-     */
     // TODO: Rename and change types and number of parameters
-    public static ScheduleFragment newInstance(String param1, String param2) {
-        ScheduleFragment fragment = new ScheduleFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static ScheduleFragment newInstance() {
+        return new ScheduleFragment();
     }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        //editScheduleInDB();
+        View view = inflater.inflate(R.layout.fragment_schedule, container, false);
+        ListView listview = view.findViewById(R.id.list_schedule);
+        CustomList adapter = new CustomList((Activity) view.getContext());
+        listview.setAdapter(adapter);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                CalendarFragment calendarFragment = new CalendarFragment();
+                transaction.replace(R.id.fragment_calendar, calendarFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        return view;
     }
+
+
+    String[] riceName = {"1","2","3"};
+
+    public class CustomList extends ArrayAdapter<String>
+    {
+        private final Activity context;
+
+
+        public CustomList(Activity context)
+        {
+            super(context, R.layout.schedule_item, riceName);
+            editScheduleInDB();
+
+            this.context = context;
+
+        }
+
+        public View getView(int position, View view, ViewGroup parent)
+        {
+            final int pos = position;
+
+            if (view == null)
+            {
+                final Context context = parent.getContext();
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.schedule_item, parent, false);
+            }
+
+            //LayoutInflater inflater = context.getLayoutInflater();
+            //View rowView = inflater.inflate(R.layout.schedule_item, null, true);
+            ImageView dot = (ImageView) view.findViewById(R.id.dot);
+            TextView schList = (TextView) view.findViewById(R.id.schedule);
+            Log.i("tag4", "44");
+            dot.setImageResource(R.drawable.ic_circle_sky);
+            schList.setText(scheduleList[position]);
+            return view;
+        }
+
+        public void editScheduleInDB() {
+            initFirebase();
+            DatabaseReference userScheduleDB = mReference.child("UserList").child(mUser.getUid()).child("userSchedule");
+            userScheduleDB.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    Map<String, Object> map = new HashMap<String, Object>();
+                    String date = "200613";
+                    for (DataSnapshot data : dataSnapshot.getChildren()) {
+                        String key = data.getKey() + "";
+                        if (date.equals(key)) {
+                            schedule = data.getValue().toString();
+                            Log.i("tag2", "78979879");
+                            scheduleList = schedule.split("/");
+                            Log.i("tag1", scheduleList[0]);
+                            Log.i("tag1", scheduleList[1]);
+                            Log.i("tag1", scheduleList[2]);
+
+                            break;
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                }
+            });
+
+        }
+
+
+    }
+
+
 }
