9968479d2ff93dbb274d94c119b4737679585419
hong3883 <hong3883@naver.com>
2020-06-16 01:59:16 +0900

    Add Logo Layout & Profile Fragment System

---
+++ app/src/main/java/com/team_comfortable/forhealthylife/LogoActivity.java
@@ -0,0 +1,39 @@
+package com.team_comfortable.forhealthylife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.team_comfortable.forhealthylife.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class LogoActivity extends AppCompatActivity
+{
+    private int timerCount = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+        final Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask()
+        {
+            @Override
+            public void run() {
+                timerCount++;
+                if(timerCount >= 2)
+                {
+                    timer.cancel();
+                    Intent intent = new Intent(getApplication(), LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.schedule(timerTask, 1000, 1000);
+    }
+}
